7e59dfd8073d32ba2e020fec23f4e05e95200d1c
Aleksandra Troshina <aatroshina@gmail.com>
2021-07-03 17:39:42 +0300

    done

--- routes/tags.js
+++ routes/tags.js
@@ -5,13 +5,11 @@ const Entry = require('../models/entry');
 
 router.get('/', async (req, res) => {
   const tags = await Tag.find();
-  console.log(tags);
+
   res.render('search', { tags });
 });
 router.post('/', async (req, res) => {
-  // console.log(req.body)
-  // const tag = await Tag.findById(req.body.tags);
-  // console.log(tag);
+
   const entries = await Entry.find({ tags: { $in: [req.body.tags] }})
 
   res.render('index', { entries });
